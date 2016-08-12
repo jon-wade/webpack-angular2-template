e2c70c913a9c56b7eaab405d11aa061e2eb601ef
jon-wade <jonwadeuk@gmail.com>
2016-08-12 08:25:34 +0100

    blank webpack-angular2 template

---
+++ src/app/app.component.spec.js
@@ -0,0 +1,13 @@
+"use strict";
+var testing_1 = require('@angular/core/testing');
+var app_component_1 = require('./app.component');
+describe('App', function () {
+    beforeEach(function () {
+        testing_1.addProviders([app_component_1.AppComponent]);
+    });
+    it('should work', testing_1.inject([app_component_1.AppComponent], function (app) {
+        // Add real test here
+        expect(2).toBe(2);
+    }));
+});
+//# sourceMappingURL=app.component.spec.js.map
